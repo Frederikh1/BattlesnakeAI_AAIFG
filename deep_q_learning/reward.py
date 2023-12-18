c3a0b509c4c8728db17edad4aceb53a813e51e15
KasperDelaxson <kaseb18@student.sdu.dk>
2023-12-18 13:01:10 +0100

    Update reward.py
    Non tested and not working code. just placeholder

--- deep_q_learning/reward.py
+++ deep_q_learning/reward.py
@@ -1,3 +1,5 @@
+# No code has been tested, boiler code from our friend. 
+
 def is_head_to_head_win(game):
     my_snake = game["you"]
     my_length = len(my_snake["length"])
@@ -12,21 +14,47 @@ def is_head_to_head_win(game):
 
 
 def is_consuming_food_low_health(game):
-    # Implement logic to check if consuming food while health is low
-    pass
+    my_snake = game["you"]
+    health = my_snake["health"]
+    food = game["board"]["food"]
+
+    # Assuming you have a way to check if food was consumed this turn
+    return health < 25 and my_snake["body"][0] in food
+
 
 def is_taking_space(game):
     # Implement logic to check if the snake is taking space effectively
     pass
 
+def is_taking_space(game):
+    # Assuming you have a flood_fill function and a way to create a board representation
+    board = create_board_representation(game)
+    start = (game["you"]["body"][0]["x"], game["you"]["body"][0]["y"])
+    area_size = flood_fill(board, start)
+
+    # Define what you consider "effectively taking space"
+    return area_size > some_threshold_value
+
 def is_consuming_food_high_health(game):
-    # Implement logic to check if consuming food while health is high
-    pass
+    my_snake = game["you"]
+    health = my_snake["health"]
+    food = game["board"]["food"]
+
+    # Assuming you have a way to check if food was consumed this turn
+    return health >= 25 and my_snake["body"][0] in food
+
 
 def is_wall_collision(game):
-    # Implement logic to check if there's a wall collision
-    pass
+    my_head = game["you"]["body"][0]
+    board_width = game["board"]["width"]
+    board_height = game["board"]["height"]
+
+    return my_head["x"] < 0 or my_head["x"] >= board_width or my_head["y"] < 0 or my_head["y"] >= board_height
+
 
 def is_self_collision(game):
-    # Implement logic to check if there's a self collision
-    pass
+    my_head = game["you"]["body"][0]
+    my_body = game["you"]["body"][1:]  # Exclude the head
+
+    return my_head in my_body
+
